b820fc50765f64de5a9fcbcb2e02d6b177faa9b3
kerem <keremkaradenzz@gmail.com>
2022-04-08 22:38:22 +0300

    add flowers data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Flowers</h1>
+        <div>
+            hello
+        </div>
+    </div>
+  );
+}
+
+export default App;
